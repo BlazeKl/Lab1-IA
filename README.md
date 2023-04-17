e41720bc4acf6e9b7ef0e9d649f8654199dd2360
BlazeKl <felipe.perezc@utem.cl>
2023-04-17 01:19:29 -0400

    Upload archivos lab

---
+++ README.md
@@ -0,0 +1,31 @@
+# laboratorio-1-IA
+
+## integrantes:
+* Felipe Perez
+* Karina Sanchez
+
+## Academico:
+* Dr. Oscar Magna
+
+## Dependencias:
+* Python 3
+* Jupyter Notebook
+* Numpy
+* Matplotlib
+* Tensorflow
+* PIL
+* Cuda
+* CuDNN
+* GPU con capacidad CUDA
+
+Las versiones de Cuda y CuDNN dependen de la version de Tensorflow que se instale.
+
+## Uso:
+### Opcion 1:
+* Ejecutar ipynb en jupyter notebook y entrenar modelos (Guardar los modelos entrenados sobreescribira los usados por server.py)
+
+### Opcion 2:
+* Ejecutar el archivo server.py con modelos ya entrenados
+
+### Una vez que python este disponible para recibir peticiones
+* Abrir web/index.html en un navegador
---
+++ server.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import io
+import numpy as np
+
+from PIL import Image
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+model = tf.keras.models.load_model('models/modelo_cnn.h5')
+model_ncnn = tf.keras.models.load_model('models/modelo_ncnn.h5')
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        print("Peticion recibida")
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        binary_image = post_data.split(b'\r\n\r\n')[1]
+
+        # Decodificar imagen a PIL
+        pil_image = Image.open(io.BytesIO(binary_image))
+        
+        # Transformar a 32x32
+        resized_image = pil_image.resize((32, 32))
+
+        # Convertir imagen PIL a numpy array
+        np_image = np.array(resized_image)
+
+        # Normalizar
+        np_image = np_image / 255.0
+
+        # Generar prediccion
+        y_pred = model.predict(np_image.reshape(1, 32, 32, 3))
+        clases = ['un avion', 'un auto', 'un ave', 'un gato', 'un ciervo', 'un perro', 'una rana', 'un caballo', 'un barco', 'un camion']
+        max = 0
+        for i in range(10):
+            if y_pred[0][i] > max:
+                max = y_pred[0][i]
+                index = i
+
+        print('Prediccion: ', clases[index])
+
+        # Generar respuesta a la petición HTTP
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        self.wfile.write(bytes(clases[index], 'utf-8'))
+
+
+print("Iniciando el servidor...")
+server = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
